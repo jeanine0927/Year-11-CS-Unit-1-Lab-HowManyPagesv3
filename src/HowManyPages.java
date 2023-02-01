2ef74e916abd13715248cbe619b588ff73009220
jeanine0927 <jc0927@student.cis.edu.hk>
2023-02-01 18:09:19 +0800

    missing src

--- src/HowManyPages.java
+++ src/HowManyPages.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
 public class HowManyPages {
   
   public static void main(String[] args) {
-    
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter your age: ");
+    int age = input.nextInt();
+    int pages = 100 - age;
+    System.out.println(age + " year olds should read at least " + pages + " pages before giving up on a book.");
     
   }
   
